af46b14c306b58f2e2b7abd3d453a21327fd07be
Tiuz90 <mattia.tiuz@hotmail.it>
2015-01-22 21:41:29 +0100

    Pg cammina, ruota e movimento camera

---
+++ Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	Vector3 initPos; 
+	Transform player;
+	// Use this for initialization
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		initPos = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate(Vector3.back / 30, Space.Self);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(Vector3.forward / 30, Space.Self);
+		}
+	}
+}
--- Assets/WoW Models/Controller_pg.cs
+++ Assets/WoW Models/Controller_pg.cs
@@ -3,38 +3,43 @@ using System.Collections;
 
 public class Controller_pg : MonoBehaviour {
 
+	float speed = 1;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		float move = Input.GetAxis ("Horizontal");
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 		//Sistema di Rotazione (non ce n'è bisogno per ora)
-		/*if (Input.GetKey (KeyCode.LeftArrow)) 
+		if (Input.GetKey (KeyCode.LeftArrow)) 
 			    transform.Rotate (Vector3.up, -1); //-1 sono i gradi di rotazione
 		else
 			if (Input.GetKey (KeyCode.RightArrow))
 					transform.Rotate (Vector3.up, 1);
-			else
-				if (Input.GetKey (KeyCode.UpArrow))
-					transform.Rotate (Vector3.left, 1);
-				else
-					if (Input.GetKey (KeyCode.DownArrow))
-						transform.Rotate (Vector3.left, -1);*/
+			
 		// FINE SISTEMA DI ROTAZIONE
 
+
 		//Usiamo i tasti per muoverci
-		if (Input.GetKey (KeyCode.W))
-			transform.Translate (Vector3.right/30, Space.Self); // diviso 30 per ridurre la velocità
+		if (Input.GetKey (KeyCode.W)) {
+			//anim.SetFloat("direction", move);
+			anim.SetBool("walk", true);
+			transform.Translate (Vector3.left * speed / 30, Space.Self); // diviso 30 per ridurre la velocità
+		}
 		if (Input.GetKey (KeyCode.S))
-			transform.Translate (Vector3.back, Space.Self);
+			transform.Translate (Vector3.right * speed / 30, Space.Self);
 		if (Input.GetKey (KeyCode.A))
-			transform.Translate (Vector3.forward, Space.Self);
+			transform.Translate (Vector3.back * speed / 30, Space.Self);
 		if (Input.GetKey (KeyCode.D))
-			transform.Translate (Vector3.back, Space.Self);
+			transform.Translate (Vector3.forward * speed / 30, Space.Self);
 		//Fine sistema di movimento semplice
 		//Ci sono gli if senza gli else per dare la possibilità di muoversi in più direzioni in una volta
+		if (Input.GetKeyUp (KeyCode.W)) {
+			anim.SetBool("walk", false);
+		}
 	}
 }
